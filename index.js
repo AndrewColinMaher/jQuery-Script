6f867f8e1ee95cae8809522acf2e8c32b801afef
AndrewColinMaher <colinmaher1710@yahoo.ie>
2021-01-03 17:57:11 +0000

    added scripts

---
+++ index.js
@@ -0,0 +1,23 @@
+$("#button-card-1").click(function () {
+    $("#par-1").toggle("slow");
+});
+$("#button-card-2").click(function () {
+    $("#par-2").toggle("medium");
+});
+$("#button-card-3").click(function () {
+    $("#par-3").toggle("fast");
+});
+$("#button-card-4").click(function () {
+    $("#par-4").toggle();
+});
+
+// $("#button-card-1").click(function () {
+//     $("#par-1").slideToggle(750);
+// });
+// $("#button-card-2").click(function () {
+//     $("#par-2").slideToggle(1000);
+// });
+// $("#button-card-3").click(function () {
+//     $("#par-3").slideToggle();
+// });
+
