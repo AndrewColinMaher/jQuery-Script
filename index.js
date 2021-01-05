0d6217884a45fd4d6867d0bc35d3585de577934a
AndrewColinMaher <colinmaher1710@yahoo.ie>
2021-01-05 15:05:14 +0000

    added chainscripts

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ $("#button-card-4").click(function () {
     $("#par-4").toggle();
 });
 
+$("button").mouseenter(function () {
+    $("button").removeClass("make-red").addClass("make-border");
+});
+$("button").mouseleave(function () {
+    $("button").removeClass("make-border").addClass("make-red");
+});
+
 // $("#button-card-1").click(function () {
 //     $("#par-1").slideToggle(750);
 // });
